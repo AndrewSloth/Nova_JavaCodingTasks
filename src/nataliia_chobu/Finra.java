6f2a796c71a26835f48a82ea79eb87bb4303ce01
nataliiachobu <mebel2008@yahoo.com>
2023-05-18 15:33:38 -0700

    added function in Finra java

--- src/nataliia_chobu/Finra.java
+++ src/nataliia_chobu/Finra.java
@@ -1,5 +1,24 @@
 package nataliia_chobu;
 
 public class Finra {
+    public static void finRa() {
+        String result = "";
+        for (int i = 1; i <= 30; i++) {
 
+            if (i % 15 == 0) {
+                result += " FINRA ";
+            } else if (i % 5 == 0) {
+                result += " RA ";
+            } else if (i % 3 == 0 ) {
+                result += " FIN ";
+
+            } else {
+                result += i + " ";
+            }
+
+
+        }
+        System.out.println(result);
+
+    }
 }
