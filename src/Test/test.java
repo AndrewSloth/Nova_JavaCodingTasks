189e43608b7dcd696bd9bed89cb24f3b49735bcd
AndrewSloth <naumovjobs@gmail.com>
2023-05-18 11:51:30 -0700

    Set up test class

---
+++ src/Test/test.java
@@ -0,0 +1,16 @@
+package Test;
+
+/*
+This class for testing your methods.
+And practicing in merge conflicts because we all able to make changes at this file.
+ */
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Nova group practise!!!");
+
+
+    }
+
+}
