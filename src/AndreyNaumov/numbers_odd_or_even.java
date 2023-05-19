4587b72bf34e0feecb31d0b6eda2ed72249868ae
Erdene_Git <erdeneuv@duck.com>
2023-05-18 22:01:25 -0700

    identify() created

--- src/AndreyNaumov/numbers_odd_or_even.java
+++ src/AndreyNaumov/numbers_odd_or_even.java
@@ -1,6 +1,7 @@
 package AndreyNaumov;
 
 
+
 /*
 Write  a method which can identify given number is even or odd
 Output ex:
@@ -8,4 +9,15 @@ identify(5); ->"Odd"
 identify(6); ->"Even"
  */
 public class numbers_odd_or_even {
+
+    public static void identify(int number){
+        if( number / 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+
+
+
 }
