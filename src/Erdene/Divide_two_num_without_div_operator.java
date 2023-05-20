12aa6aa8564708038b29e892f6845680eedce049
Erdene_Git <erdeneuv@duck.com>
2023-05-20 12:13:44 -0700

    Added Divide()

---
+++ src/Erdene/Divide_two_num_without_div_operator.java
@@ -0,0 +1,24 @@
+package Erdene;
+
+public class Divide_two_num_without_div_operator {
+    public static void Divide(double Dividend, double divisor) {
+        int count = 1;
+        double div = Dividend;
+
+        try {
+            if (divisor == 0 || Dividend < 0 || divisor > Dividend) {
+                throw new Exception("Can't divide number by ZERO, or number that's smaller than Dividend, or divide negative number with this method");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        while(Dividend > divisor){
+            Dividend -= divisor;
+            count ++;
+        }
+        System.out.println(div + " / " + divisor + " = "+ count);
+
+    }
+
+}
