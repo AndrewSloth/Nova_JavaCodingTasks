e06ec6e8ecc2871c0469f5810ad776110c0a6dd6
akmalsalomov <akmalsalomov@gmail.com>
2023-05-21 06:37:32 +0700

    FINRA

---
+++ src/akmalSalomov/OddOrEven.java
@@ -0,0 +1,28 @@
+package akmalSalomov;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        oddOrEven(5);
+
+    }
+
+    public static void oddOrEven (int number){
+        if (number % 2 == 0){
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd number");
+        }
+    }
+
+}
+/*
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+ */
+/*
+NDivide without / operator:numbers -
+Write a method that can divide two numbers without using division operator
+ */
