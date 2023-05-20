b2d3b003a4b4f1eb753164afd30c96017812c577
Erdene_Git <erdeneuv@duck.com>
2023-05-20 12:14:58 -0700

    Run some test

--- src/Test/test.java
+++ src/Test/test.java
@@ -9,10 +9,21 @@ This class for testing your methods.
 And practicing in merge conflicts because we all will be able to make changes at this file.
  */
 
+import AndreyNaumov.finra;
+import Erdene.Divide_two_num_without_div_operator;
+import Erdene.Finra;
+import Erdene.Numbers_odd_or_even;
+
 public class test {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Nova group practise!!!");
+
+        Numbers_odd_or_even.identify(11);
+
+
+        Divide_two_num_without_div_operator.Divide(15,5);
+
 
 
     }
