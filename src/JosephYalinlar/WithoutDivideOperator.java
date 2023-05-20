8d7fdb1011cd50f48e225aef63d43c1f2c8ccdd9
yusufyalinlar <yusufyalinlar@gmail.com>
2023-05-19 21:50:00 -0700

    Week 1 Tasks

---
+++ src/JosephYalinlar/WithoutDivideOperator.java
@@ -0,0 +1,9 @@
+package JosephYalinlar;
+
+public class WithoutDivideOperator {
+
+    public int divideWithoutDivideOperator(int number, int divideBy) {
+
+        return number / divideBy;
+    }
+}
