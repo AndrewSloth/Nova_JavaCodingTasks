9468f7dd34c6fa574d993f364b84e00fb48e19ef
AndrewBalint51 <andrew.balint@hotmail.com>
2023-05-20 17:21:10 -0700

    Added FINRA

--- src/AndrewBalint/TaskUtilityMethods.java
+++ src/AndrewBalint/TaskUtilityMethods.java
@@ -9,6 +9,8 @@ public class TaskUtilityMethods {
 
         System.out.println(NDivide(99,25));
 
+        FINRA();
+
     }
 
     public static String identify(int num){
@@ -50,6 +52,34 @@ Write a method that can divide two numbers without using division operator
          */
     }
 
+    public static void FINRA(){
 
+        for(int i =1; i<=30;i++){
+
+            if(i%3==0){
+                if(i%5==0){
+                    System.out.print("FINRA ");
+                }
+                else{
+                    System.out.print("FIN ");
+                }
+
+            }
+            else if(i%5==0){
+                System.out.print("RA ");
+            }
+            else{
+                System.out.print(i+" ");
+            }
+        }
+
+        /*
+        FINRA:
+            Write a function which prints out the numbers from 1 to 30 but for numbers
+            which are a multiple of 3, print "FIN" instead of the number and for numbers
+            which are a multiple of 5, print "RA" instead of the number. For numbers which are a
+            multiple of both 3 and 5, print "FINRA" instead of the number.
+         */
+    }
 
 }
