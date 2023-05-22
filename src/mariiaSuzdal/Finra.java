3b7655325f1945bc8dde095bb05c2d113599f504
maria_suzdal <m.suzdal11@gmail.com>
2023-05-21 21:14:19 -0700

    added "FINRA" task solution

--- src/mariiaSuzdal/Finra.java
+++ src/mariiaSuzdal/Finra.java
@@ -3,6 +3,27 @@ package mariiaSuzdal;
 public class Finra {
     public static void main(String[] args) {
 
+        finra();
+
+    }
+
+    public static void finra() {
+
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+
     }
 }
 /*
