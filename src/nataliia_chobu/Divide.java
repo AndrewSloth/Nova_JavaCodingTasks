9194b2640bd7d88a3bbdbed95b6a16241cc9692d
nataliiachobu <mebel2008@yahoo.com>
2023-05-21 21:27:58 -0700

    Added System.exit() to  Divide.java

--- src/nataliia_chobu/Divide.java
+++ src/nataliia_chobu/Divide.java
@@ -2,17 +2,19 @@ package nataliia_chobu;
 
 public class Divide {
     public static void main(String[] args) {
-divideWithoutDivisionOperator(12,5);
+divideWithoutDivisionOperator(-12,5);
 
     }
 
 
     public static void divideWithoutDivisionOperator(int a,int b) {
 
-        int result = 0;
-        if(a<=0 || b<=0){
-            System.err.println("Zero and negative numbers are not accepted. Given dividend is \""+a+"\", given divisor is \""+b+"\".");
+
+        if(a<=0 || b<=0) {
+            System.err.println("Zero and negative numbers are not accepted. Given dividend is \"" + a + "\", given divisor is \"" + b + "\".");
         }
+        System.exit(0);
+        int result = 0;
             while (a >= b) {
 
                 a -= b;
