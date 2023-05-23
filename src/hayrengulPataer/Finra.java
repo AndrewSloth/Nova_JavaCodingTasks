ff2740cf523ceb92b3376451bb5f1434c58083d7
heyrigulpattar <heyrigulp@gmail.com>
2023-05-22 20:23:33 -0700

    Finra file added

---
+++ src/hayrengulPataer/Finra.java
@@ -0,0 +1,23 @@
+package hayrengulPataer;
+
+public class Finra {
+    public static void main(String[] args) {
+        String result = "";
+
+        for( int i = 1; i < 30; i++){
+            if( i % 15 == 0) {
+                result += "FINRA ";
+            } else if(i % 3== 0){
+                result += "FIn ";
+            }else if(i % 5 == 0){
+                result += "RA ";
+            }else{
+                result += i + " ";
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
+
