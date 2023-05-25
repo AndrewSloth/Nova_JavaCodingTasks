159016947ebba6f4c349d0b7ccd35fee56ecf7b5
nataliiachobu <mebel2008@yahoo.com>
2023-05-25 09:14:52 -0700

    modified SwapNumbers.java.

--- src/nataliia_chobu/week2/SwapNumbers.java
+++ src/nataliia_chobu/week2/SwapNumbers.java
@@ -3,14 +3,15 @@ package nataliia_chobu.week2;
 public class SwapNumbers {
     public static void main(String[] args) {
 
-        int a=5;//after swap a=9
-        int b=9;//after swap b=5
-
-        a=a+b;//5+9=14
-        b=a-b;//14-9=5
-        a=a-b;//14-5=9
-        System.out.println("a = "+a+", b = "+b);
-
+     swapNumbers(5,9);
 
     }
+    public static void swapNumbers(int a,int b) {
+        a = a + b;//5+9=14
+        b = a - b;//14-9=5
+        a = a - b;//14-5=9
+        System.out.println("a = " + a + ", b = " + b);
+
+    }
+
 }
