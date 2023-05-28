64aa2d56bfbfcbe4faa8533e407b8e78c842d9bb
maria_suzdal <m.suzdal11@gmail.com>
2023-05-27 23:01:27 -0700

    added a solution to the task Swap numbers

--- src/mariiaSuzdal/week_2/SwapNumbers.java
+++ src/mariiaSuzdal/week_2/SwapNumbers.java
@@ -1,4 +1,22 @@
 package mariiaSuzdal.week_2;
 
 public class SwapNumbers {
+    public static void main(String[] args) {
+
+        numbers(10, 5);
+
+
+    }
+
+    public static void numbers(int x, int y) {
+        System.out.println("x base: " + x);
+        System.out.println("y base: " + y);
+
+        y = y + x; //15
+        x = y - x; // 5
+        y = y - x;
+
+        System.out.println("x swap: " + x);
+        System.out.println("y swap: " + y);
+    }
 }
