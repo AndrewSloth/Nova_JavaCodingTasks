cb791230b05bbbf02b91d46f52128511586dbea9
maria_suzdal <m.suzdal11@gmail.com>
2023-05-27 22:53:58 -0700

    added a solution to the task Divisible by numbers

--- src/mariiaSuzdal/week_2/DivisibleByNumbers.java
+++ src/mariiaSuzdal/week_2/DivisibleByNumbers.java
@@ -1,4 +1,31 @@
 package mariiaSuzdal.week_2;
 
 public class DivisibleByNumbers {
+    public static void main(String[] args) {
+        divisible();
+    }
+
+    public static void divisible() {
+
+        String byFifty = "";
+        String byFive = "";
+        String byThree = "";
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                byFifty += i + " ";
+            } else if (i % 3 == 0) {
+                byThree += i + " ";
+            } else if (i % 5 == 0) {
+                byFive += i + " ";
+            }
+
+        }
+
+        System.out.println(byFifty);
+        System.out.println(byThree);
+        System.out.println(byFive);
+
+    }
+
 }
