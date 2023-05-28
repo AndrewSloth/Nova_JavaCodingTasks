e3fc9575e0ed04b6de9c417f6f4dc7b755116e19
Nonos1500 <erdeneuv@duck.com>
2023-05-28 15:25:02 -0700

    Tested

--- src/Erdene/week02/ConsectiveNumbers.java
+++ src/Erdene/week02/ConsectiveNumbers.java
@@ -1,29 +1,49 @@
 package Erdene.week02;
 
 public class ConsectiveNumbers {
-    public static void consecNumber(int N ){
 
-        for (int i = 1; i < N; i++) {
+            public static void printConsecutiveNumbers(int N) {
 
-            String res = "";
-
-            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
-                if (i % 2 == 0) {
-                    res += "Codility";
+                try {
+                    if (N < 1) {
+                        throw new Exception("Please enter positive numbers");
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-                if (i % 3 == 0) {
-                    res += "Test";
-                }
-                if (i % 5 == 0) {
-                    res += "Coders";
-                }
-            } else {
-                res = "" + i;
-            }
 
-            System.out.println(res);
 
-             }
+                for (int i = 1; i < N; i++) {
+
+                    if (i % 2 == 0 && i % 3 == 0) {
+                        System.out.println("CodilityTest");
+                        continue;
+                    }
+
+                    if (i % 2 == 0 && i % 5 == 0) {
+                        System.out.println("CodilityCoders");
+                        continue;
+                    }
+
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        System.out.println("TestCoders");
+                        continue;
+                    }
+
+                    if (i % 2 == 0) {
+                        System.out.println("Codility");
+                        continue;
+                    }
+                    if (i % 3 == 0) {
+                        System.out.println("Test");
+                        continue;
+                    }
+                    if (i % 5 == 0) {
+                        System.out.println("Coders");
+                        continue;
+                    }
+                    System.out.println(i);
 
         }
+    }
 }
