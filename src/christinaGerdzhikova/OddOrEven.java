459a7eb4ec88ceb8c309218b04d267a6301ad414
Chris_Cydeo <praskofka@hotmail.com>
2023-05-29 16:06:04 -0700

    Finished OddOrEven class

--- src/christinaGerdzhikova/OddOrEven.java
+++ src/christinaGerdzhikova/OddOrEven.java
@@ -1,4 +1,19 @@
 package christinaGerdzhikova;
 
+import java.util.Scanner;
+
 public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(OddOrEvenNum(scan.nextInt()));
+    }
+
+    public static String OddOrEvenNum (int num){
+
+        if (num % 2 == 0){
+            return "Even";
+        }
+        return "Odd";
+    }
 }
