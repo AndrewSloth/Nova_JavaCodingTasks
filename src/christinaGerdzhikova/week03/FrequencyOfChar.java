dc7475b07b2a759bc0f41a4eacb38959239dbaf1
Chris_Cydeo <praskofka@hotmail.com>
2023-05-31 13:12:17 -0700

    finished frequency of char class

--- src/christinaGerdzhikova/week03/FrequencyOfChar.java
+++ src/christinaGerdzhikova/week03/FrequencyOfChar.java
@@ -1,4 +1,40 @@
 package christinaGerdzhikova.week03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class FrequencyOfChar {
+
+    public static String frequencyOfChar(String word){
+
+        char[] arr = word.toCharArray();
+
+        String result = "";
+
+        for (char each : arr) {
+            int count = 0;
+            for (char element : arr) {
+                if (each == element){
+                    count++;
+                }
+            }
+            if (result.contains("" + each)){
+                continue;
+            }
+            result += "" + each + count;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String word = scan.next();
+
+        System.out.println(frequencyOfChar(word));
+    }
 }
