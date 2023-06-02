3ea5df2450401967c2ce0cd29c32690e531be148
maria_suzdal <m.suzdal11@gmail.com>
2023-06-02 00:49:34 -0700

    corrected a solution for task ReverseNegativeNumber

--- src/mariiaSuzdal/week_3/ReverseNegativeNumber.java
+++ src/mariiaSuzdal/week_3/ReverseNegativeNumber.java
@@ -2,23 +2,23 @@ package mariiaSuzdal.week_3;
 
 public class ReverseNegativeNumber {
     public static void main(String[] args) {
+
         System.out.println(number(-100));
     }
 
     public static int number(int num) {
 
-
-        int numberreturn = 0;
+        int numberReturn = 0;
 
         if (num < 0) {
-            numberreturn -= num;
+            numberReturn -= num;
         } else {
             System.out.println("You should try with negative number");
             System.exit(1);
-            // numberreturn += num;
+            // numberReturn += num;
         }
+        return numberReturn;
 
-        return numberreturn;
     }
 }
 /*
