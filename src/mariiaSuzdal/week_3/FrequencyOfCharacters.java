c2b2e29afaf3f16ff8eab8b559e7d057d1583c7a
maria_suzdal <m.suzdal11@gmail.com>
2023-06-02 00:05:35 -0700

    added a solution for task FrequencyOfCharacters

--- src/mariiaSuzdal/week_3/FrequencyOfCharacters.java
+++ src/mariiaSuzdal/week_3/FrequencyOfCharacters.java
@@ -1,6 +1,33 @@
 package mariiaSuzdal.week_3;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+        System.out.println(freqOfChar("AAABBCDDDD"));
+
+    }
+
+    public static String freqOfChar(String characters) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList(characters.split("")));
+
+        String answer = "";
+
+        for (String each : list) {
+            int frequency = Collections.frequency(list, each);
+
+            if (!(answer.contains(each))) {
+                answer += each + frequency;
+            }
+        }
+
+        return answer;
+    }
 }
 /*
 Write a return method that can find the frequency of characters
