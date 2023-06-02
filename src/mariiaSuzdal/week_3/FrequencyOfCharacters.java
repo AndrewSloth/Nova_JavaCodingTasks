e877dc3a1d974a48ccba9eec0faff2ab78d7efb6
maria_suzdal <m.suzdal11@gmail.com>
2023-06-01 23:18:22 -0700

    added a new task and description Frequency of Characters

---
+++ src/mariiaSuzdal/week_3/FrequencyOfCharacters.java
@@ -0,0 +1,8 @@
+package mariiaSuzdal.week_3;
+
+public class FrequencyOfCharacters {
+}
+/*
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2**
+ */
