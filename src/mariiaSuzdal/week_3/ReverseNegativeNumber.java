75ba1d8ec2fab16a2e5d752598f433262852ec7a
maria_suzdal <m.suzdal11@gmail.com>
2023-06-02 00:47:24 -0700

    add a solution for task ReverseNegativeNumber

--- src/mariiaSuzdal/week_3/ReverseNegativeNumber.java
+++ src/mariiaSuzdal/week_3/ReverseNegativeNumber.java
@@ -1,6 +1,25 @@
 package mariiaSuzdal.week_3;
 
 public class ReverseNegativeNumber {
+    public static void main(String[] args) {
+        System.out.println(number(-100));
+    }
+
+    public static int number(int num) {
+
+
+        int numberreturn = 0;
+
+        if (num < 0) {
+            numberreturn -= num;
+        } else {
+            System.out.println("You should try with negative number");
+            System.exit(1);
+            // numberreturn += num;
+        }
+
+        return numberreturn;
+    }
 }
 /*
 Write a return method that can reverse negative number and return it as int
