746b76772c2716d4cd5ea8bfcaabc37a3f4cdd67
maria_suzdal <m.suzdal11@gmail.com>
2023-06-02 10:13:31 -0700

    corrected a solution for task FrequencyOfCharacters

--- src/mariiaSuzdal/week_3/FrequencyOfCharacters.java
+++ src/mariiaSuzdal/week_3/FrequencyOfCharacters.java
@@ -1,13 +1,14 @@
 package mariiaSuzdal.week_3;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class FrequencyOfCharacters {
     public static void main(String[] args) {
+
         System.out.println(freqOfChar("AAABBCDDDD"));
+
+        System.out.println(freqOfChar2("AAABBCDDDD"));
 
     }
 
@@ -26,6 +27,25 @@ public class FrequencyOfCharacters {
         }
 
         return answer;
+    }
+
+    public static Map freqOfChar2(String char2) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i <= char2.length() -1; i++) {
+
+            if (map.containsKey(char2.charAt(i))) {
+                map.replace(char2.charAt(i), map.get(char2.charAt(i)) + 1);
+
+            } else {
+                map.put(char2.charAt(i), 1);
+            }
+
+        }
+        return map;
+
     }
 }
 /*
