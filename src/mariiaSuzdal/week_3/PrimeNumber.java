8a897e6c3422ea2a359c130bd8b913d3c4c20bc5
maria_suzdal <m.suzdal11@gmail.com>
2023-06-02 10:14:29 -0700

    added a solution for task PrimeNumber

--- src/mariiaSuzdal/week_3/PrimeNumber.java
+++ src/mariiaSuzdal/week_3/PrimeNumber.java
@@ -1,6 +1,31 @@
 package mariiaSuzdal.week_3;
 
 public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(number(3));
+
+    }
+
+    public static boolean number(int num) {
+
+        if (num < 1) {
+            return false;
+        }
+
+
+        for (int i = 2; i <= num - 1; i++) {
+
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 }
 /*
 Write a method that can check if a number is prime or not
