7df8cbc39c2da84c95677b49529cfa4b034e815d
AndrewBalint51 <andrew.balint@hotmail.com>
2023-06-03 20:35:03 -0700

    Finished isPrime and reverseNegative

--- src/AndrewBalint/week3/Week3Utility.java
+++ src/AndrewBalint/week3/Week3Utility.java
@@ -1,7 +1,20 @@
 package AndrewBalint.week3;
 
+import java.util.InputMismatchException;
+
 public class Week3Utility {
     public static void main(String[] args) {
+
+        //System.out.println(isPrime(31));
+
+        try{
+            System.out.println(reverseNegative(14));;
+        }
+        catch(InputMismatchException e){
+            e.printStackTrace();
+        }
+
+
 
     }
 
@@ -13,10 +26,17 @@ public class Week3Utility {
             Write a method that can check if a number is prime or not
          */
 
-        boolean isPrime = false;
+        //Prime numbers must be greater than one
+        if(num<=1){
+            return false;
+        }
+
+        boolean isPrime = true;
 
         for(int i = 2; i <num; i++){
-
+            if(num%i==0) {
+                isPrime = false;
+            }
         }
 
 
@@ -31,6 +51,12 @@ public class Week3Utility {
          2) Number - Reverse negative number
             Write a return method that can reverse negative number and return it as int
          */
+
+        if(neg>=0){
+            throw new InputMismatchException("Invalid input. Input must be a negative number.");
+        }
+
+        neg = neg*(-1);
 
         return neg;
     }
