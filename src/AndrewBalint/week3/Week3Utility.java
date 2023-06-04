fd29771a30b80899a3bf63a0cfccab09a3e8099e
AndrewBalint51 <andrew.balint@hotmail.com>
2023-06-03 20:23:55 -0700

    Added Week3 package and UtilityClass. Began isPrime class.

---
+++ src/AndrewBalint/week3/Week3Utility.java
@@ -0,0 +1,48 @@
+package AndrewBalint.week3;
+
+public class Week3Utility {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean isPrime(int num) {
+
+        /*
+        1)  Numbers - Prime Number
+            Write a method that can check if a number is prime or not
+         */
+
+        boolean isPrime = false;
+
+        for(int i = 2; i <num; i++){
+
+        }
+
+
+
+        return isPrime;
+
+    }
+
+    public static int reverseNegative(int neg){
+
+        /*
+         2) Number - Reverse negative number
+            Write a return method that can reverse negative number and return it as int
+         */
+
+        return neg;
+    }
+
+    public static String frequencyOfChars(String entry){
+        /*
+            3) String - Frequency of Characters
+            Write a return method that can find the frequency of characters
+            Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2**
+         */
+        String result ="";
+
+        return result;
+    }
+}
