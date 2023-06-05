fbf9203707d9a03da30d66397a010705fec62350
yusufyalinlar <yusufyalinlar@gmail.com>
2023-06-05 15:47:20 -0700

    Week 1 Tasks

--- src/JosephYalinlar/week1/WithoutDivideOperator.java
+++ src/JosephYalinlar/week1/WithoutDivideOperator.java
@@ -1,9 +1,48 @@
+/*
+Write a function that divides without using a division operator
+ */
+
 package JosephYalinlar.week1;
+
+import java.util.Scanner;
 
 public class WithoutDivideOperator {
 
-    public int divideWithoutDivideOperator(int number, int divideBy) {
+    public static int divideWithoutDivideOperator(int number, int divideBy) {
 
-        return number / divideBy;
+        int divided = 0;
+        int dividedNum;
+
+        if (divideBy == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+
+        if (number > 0) {
+            do {
+                dividedNum = number - divideBy;
+                if (dividedNum >= 0) {
+                    number = dividedNum;
+                    divided++;
+                }
+            } while (dividedNum >= 0);
+        }
+
+        return divided;
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your number to be divided:");
+        int num = s.nextInt();
+
+        System.out.println("Please enter your number you want to divide by:");
+        int divideBy = s.nextInt();
+
+        System.out.println("Your result is: " + divideWithoutDivideOperator(num, divideBy));
+        s.close();
     }
 }
+
+
---
+++ src/JosephYalinlar/week3/PrimeNum.java
@@ -0,0 +1,21 @@
+/*
+Write a method that can check if a number is prime or not
+ */
+
+
+package JosephYalinlar.week3;
+
+public class PrimeNum {
+    public boolean checkPrimeNum(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
