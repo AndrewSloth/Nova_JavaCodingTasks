935e6c7afa661a44234a665672a6bec98302bd27
Nonos1500 <erdeneuv@duck.com>
2023-06-04 17:15:48 -0700

    Updated for negative numbers.

--- src/Erdene/week03/Numbers_PrimeNumber.java
+++ src/Erdene/week03/Numbers_PrimeNumber.java
@@ -5,6 +5,11 @@ public class Numbers_PrimeNumber {
 
     public static void primeNumber(int N){
         boolean notPrime = false;
+
+        if(N<=1){
+            notPrime = true;
+        }
+
         for (int i = 2; i < N; i++) {
             if (N % i == 0) {
                 notPrime = true;
@@ -15,12 +20,12 @@ public class Numbers_PrimeNumber {
         if(!notPrime){
                 System.out.println(N + " is a Prime Number");
             }else{
-                System.out.println(N + " is not a Prime Number");
+                System.out.println(N + " is NOT a Prime Number");
             }
         }
 
 
     public static void main(String[] args) {
-        primeNumber(29);
+        primeNumber(-29);
     }
 }
