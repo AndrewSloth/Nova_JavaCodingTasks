cd297fc4987b96d6b1b5eb6b46b018cc6b5764df
maria_suzdal <m.suzdal11@gmail.com>
2023-06-10 15:30:30 -0700

    created a new file for the SameLetter task

---
+++ src/mariiaSuzdal/week_4/SameLetters.java
@@ -0,0 +1,12 @@
+package mariiaSuzdal.week_4;
+
+public class SameLetters {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Write a return method that check if a string is build out of the same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+ */
