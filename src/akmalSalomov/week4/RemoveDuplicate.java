1e88ade527e559fc70579b95208f5b8d952bb0dd
akmalsalomov <akmalsalomov@gmail.com>
2023-06-10 08:16:31 -0400

    solution for remove duplicates (made some changes)

--- src/akmalSalomov/week4/RemoveDuplicate.java
+++ src/akmalSalomov/week4/RemoveDuplicate.java
@@ -10,6 +10,14 @@ public class RemoveDuplicate {
                 result += chr;
             }
         }return result;
+        /*
+        So in this method takes String as input and returns modified String with all duplicates removed.
+        We created and empty String 'result' and looped through each character in str. For each character
+        we checked if it is already present in the 'result' String using indexOf method. If it is not present
+        it adds it to the result String. This makes sure that only first appearance of each char is added to
+        result, and it returns the result with all duplicates removed.
+
+         */
     }
 
     public static void main(String[] args) {
