e7dc9cb013432cbb7b1e3a13ac77f2a90ede9925
maria_suzdal <m.suzdal11@gmail.com>
2023-06-10 15:30:50 -0700

    created a new file for the RemoveDuplicates task

---
+++ src/mariiaSuzdal/week_4/RemoveDuplicates.java
@@ -0,0 +1,12 @@
+package mariiaSuzdal.week_4;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Write a return method that can remove the duplicated values from String
+Ex: removeDup("AAABBBCCC") ==> ABC
+
+ */
