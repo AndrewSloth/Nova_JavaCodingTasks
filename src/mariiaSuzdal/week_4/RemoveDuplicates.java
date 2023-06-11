0d7904e16c0889cb5be044b1a173cb90816d36ee
maria_suzdal <m.suzdal11@gmail.com>
2023-06-11 12:32:50 -0700

    added a solution for the RemoveDuplicates task

--- src/mariiaSuzdal/week_4/RemoveDuplicates.java
+++ src/mariiaSuzdal/week_4/RemoveDuplicates.java
@@ -8,6 +8,7 @@ public class RemoveDuplicates {
 
         System.out.println(removeDuplicates1("AAABBBCCC"));
         System.out.println(removeDuplicates2("AAAFFFBBBAAACCDDCD"));
+        System.out.println(removeDuplicates3("AAAFFFBBBAAACCDDCD"));
 
     }
 
@@ -28,12 +29,29 @@ public class RemoveDuplicates {
     //secondSolution
     public static String removeDuplicates2(String str) {
 
-        // ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split("")));
-        //Collections.sort(list);
-
         Set<String> set1 = new TreeSet<>(List.of(str.split("")));
 
         return set1.toString();
+    }
+
+    //thirdSolution
+
+    public static String removeDuplicates3(String str) {
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split("")));
+        Collections.sort(list);
+
+        ArrayList<String> list2 = new ArrayList<>();
+
+        for (String each : list) {
+            if (list2.contains(each)) {
+                continue;
+            }
+            list2.add(each);
+        }
+
+        return list2.toString();
+
     }
 
 
