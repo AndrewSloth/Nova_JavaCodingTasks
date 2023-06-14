3e369e6198098cd13f3cedb5aa2733a7a7495311
yusufyalinlar <yusufyalinlar@gmail.com>
2023-06-13 18:12:26 -0700

    Week 4Tasks

--- src/JosephYalinlar/week3/week3Test.java
+++ src/JosephYalinlar/week3/week3Test.java
@@ -9,13 +9,21 @@ public class week3Test {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number to see if your number if it's a prime number");
         int num = scanner.nextInt();
-        System.out.println("Is your number a prime number? >" + PrimeNum.checkPrimeNum(num));
-//-----------------------------------------------------------------------
+        System.out.println("Is your number a prime number? >" + PrimeNum.checkPrimeNum(num)+"\n" +
+                "-----------------------------------------------------------------------");
+
         ReverseNegativeNum RevNegNum = new ReverseNegativeNum();
         System.out.println("Please enter your negative number to be reversed");
         int num2 = scanner.nextInt();
-        System.out.println("Your new num is " + RevNegNum.reverseNegativeNum(num2));
-//-----------------------------------------------------------------------
+
+        System.out.println("Your new num is " + RevNegNum.reverseNegativeNum(num2)+"\n" +
+                "-----------------------------------------------------------------------");
+        FrequencyOfChar frequencyOfChar = new FrequencyOfChar();
+        System.out.println("To see how many time a char appears in your string enter a string:");
+        String str = scanner.nextLine();
+        scanner.next();
+        String result = frequencyOfChar.checkFrequencyOfChar(str);
+        System.out.println("Your char frequencies are: "+ result);
 
     }
 }
