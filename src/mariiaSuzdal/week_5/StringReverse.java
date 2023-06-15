061ea2c433505d7aef0578a8d9d389f81a09820b
maria_suzdal <m.suzdal11@gmail.com>
2023-06-14 18:17:10 -0700

    added a new file for the StringReverse task

---
+++ src/mariiaSuzdal/week_5/StringReverse.java
@@ -0,0 +1,14 @@
+package mariiaSuzdal.week_5;
+
+public class StringReverse {
+
+    public static void main(String[] args) {
+
+    }
+}
+/*
+
+Write a return method that can reverse String
+Ex: Reverse("ABCD"); ==> DCBA
+
+ */
