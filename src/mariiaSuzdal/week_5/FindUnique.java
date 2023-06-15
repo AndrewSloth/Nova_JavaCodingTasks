f0fdf8cf3cb18e6ba779fc2b372f46ca76801454
maria_suzdal <m.suzdal11@gmail.com>
2023-06-14 18:16:57 -0700

    added a new file for the FindUnique task

---
+++ src/mariiaSuzdal/week_5/FindUnique.java
@@ -0,0 +1,13 @@
+package mariiaSuzdal.week_5;
+
+public class FindUnique {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+
+Write a return  method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+
+ */
