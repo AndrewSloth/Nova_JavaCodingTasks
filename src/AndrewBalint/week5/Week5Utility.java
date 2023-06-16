420b948e33526c867646da99bd96ef0bee542628
AndrewBalint51 <andrew.balint@hotmail.com>
2023-06-15 21:20:52 -0700

    Finished returnUniqueChars and two versions reverseString

---
+++ src/AndrewBalint/week5/Week5Utility.java
@@ -0,0 +1,74 @@
+package AndrewBalint.week5;
+
+public class Week5Utility {
+
+    public static void main(String[] args) {
+
+        System.out.println(returnUniqueChars("ABBBCCCDEFAF1 1 1 23"));
+        System.out.println(reverseStringVersion1("stunod ekil I"));
+        System.out.println(reverseStringVersion2("nuf si avaJ"));
+    }
+
+    /*
+    3.String -- Find the unique
+    Write a return  method that can find the unique characters from the String
+    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+     */
+    public static String returnUniqueChars(String str){
+
+        //The resulting String with unique chars
+        String uniqueChars ="";
+
+        //iterate through each character
+        for(char each : str.toCharArray()){
+
+            //How many times the character has appeared
+            int count = 0;
+
+            //iterate through the string a second time and see how many times the character reappears
+            for (char dupeCheck: str.toCharArray()) {
+
+                //Increase count each time the character is encountered
+                if(dupeCheck==each){
+                    count++;
+                }
+            }
+
+            //If char is only found once it is unique and we add to the result String
+            if(count==1){
+                uniqueChars+=""+each;
+            }
+
+        }
+
+        return uniqueChars;
+    }
+
+    /*
+    4) String - Reverse
+Write a return method that can reverse String
+Ex: Reverse("ABCD"); ==> DCBA
+     */
+
+    //This version uses StringBuilder
+    public static String reverseStringVersion1(String str){
+
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    //This version iterates through the String
+    public static String reverseStringVersion2(String str){
+
+        String reversed = "";
+
+        //Iterate through String in reverse and append each char to new String
+        for(int i = str.length()-1; i>=0; i--){
+            reversed+=""+str.charAt(i);
+        }
+
+        return reversed;
+
+
+    }
+
+}
