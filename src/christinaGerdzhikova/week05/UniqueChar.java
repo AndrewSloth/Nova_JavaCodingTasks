cbbf2b12324853ecd8c86e2c48fea045d333e2bc
Chris_Cydeo <praskofka@hotmail.com>
2023-06-23 10:01:38 -0700

    finished unique char class

--- src/christinaGerdzhikova/week05/UniqueChar.java
+++ src/christinaGerdzhikova/week05/UniqueChar.java
@@ -5,4 +5,28 @@ public class UniqueChar {
     //3.String -- Find the unique
     //Write a return  method that can find the unique characters from the String
     //Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+
+    public static String uniqueChar(String word){
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(i) == word.charAt(j)){
+                    count++;
+                }
+            }
+            if (count == 1) {
+                result += word.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String word = "AAABBBCCCDEF";
+
+        System.out.println("uniqueChar(word) = " + uniqueChar(word));
+    }
 }
