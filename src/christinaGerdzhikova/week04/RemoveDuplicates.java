39dfacdba6304048c845af3cd540113c2d580b8a
Chris_Cydeo <praskofka@hotmail.com>
2023-06-23 09:13:52 -0700

    finished remove duplicates class

--- src/christinaGerdzhikova/week04/RemoveDuplicates.java
+++ src/christinaGerdzhikova/week04/RemoveDuplicates.java
@@ -6,4 +6,26 @@ public class RemoveDuplicates {
     //Write a return method that can remove the duplicated values from String
     //Ex: removeDup("AAABBBCCC") ==> ABC
 
+    public static String removeDuplicatesMethod(String word){
+
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (result.contains("" + word.charAt(i))){
+                continue;
+            }else {
+                result += word.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String word = "AAABBBCCC";
+
+        System.out.println("removeDuplicatesMethod(word) = " + removeDuplicatesMethod(word));
+    }
+
 }
