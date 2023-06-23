e6f23ca09ee5faed188b86003a3090b41d71e19e
Chris_Cydeo <praskofka@hotmail.com>
2023-06-23 08:45:03 -0700

    Created remove duplicates class

---
+++ src/christinaGerdzhikova/week04/RemoveDuplicates.java
@@ -0,0 +1,9 @@
+package christinaGerdzhikova.week04;
+
+public class RemoveDuplicates {
+
+    //String -- Remove Duplicates
+    //Write a return method that can remove the duplicated values from String
+    //Ex: removeDup("AAABBBCCC") ==> ABC
+
+}
