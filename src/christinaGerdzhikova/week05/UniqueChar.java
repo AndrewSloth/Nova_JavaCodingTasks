a977f429c5d55e61dafb6dbdbc14aeabda9a3584
Chris_Cydeo <praskofka@hotmail.com>
2023-06-23 09:23:05 -0700

    created unique char class

---
+++ src/christinaGerdzhikova/week05/UniqueChar.java
@@ -0,0 +1,8 @@
+package christinaGerdzhikova.week05;
+
+public class UniqueChar {
+
+    //3.String -- Find the unique
+    //Write a return  method that can find the unique characters from the String
+    //Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+}
