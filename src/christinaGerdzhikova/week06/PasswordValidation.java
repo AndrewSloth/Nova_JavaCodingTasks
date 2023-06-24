b65a5a501622eb0c76958532ec25a3660130357b
Chris_Cydeo <praskofka@hotmail.com>
2023-06-24 13:08:12 -0700

    finished password validation class

--- src/christinaGerdzhikova/week06/PasswordValidation.java
+++ src/christinaGerdzhikova/week06/PasswordValidation.java
@@ -10,4 +10,39 @@ public class PasswordValidation {
     //4. Password should at least contain one special characters
     //5. Password should at least contain a digit
     //6. if all requirements above are met, the method returns true, otherwise returns false
+
+    public static boolean passwordValidation(String password){
+
+        int upperCaseCount = 0;
+        int lowerCaseCount = 0;
+        int specialCharCount = 0;
+        int digitCount = 0;
+
+        if (password.length() >= 6 && !(password.contains(" "))){
+
+            for (int i = 0; i < password.length(); i++) {
+
+                if (Character.isDigit(password.charAt(i))){
+                    digitCount++;
+                }
+                if (Character.isUpperCase(password.charAt(i))){
+                    upperCaseCount++;
+                }
+                if (Character.isLowerCase(password.charAt(i))){
+                    lowerCaseCount++;
+                }
+                if (!(Character.isLetterOrDigit(password.charAt(i)))){
+                    specialCharCount++;
+                }
+            }
+            if (upperCaseCount >= 1 && lowerCaseCount >= 1 && specialCharCount >= 1 && digitCount >= 1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("passwordValidation(\"Coon2018!\") = " + passwordValidation("ru6QW2@"));
+    }
 }
