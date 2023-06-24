05beb6ef8f3799ca7c4a0a8cf694d27b7069881b
Chris_Cydeo <praskofka@hotmail.com>
2023-06-24 11:09:31 -0700

    created password validation class

---
+++ src/christinaGerdzhikova/week06/PasswordValidation.java
@@ -0,0 +1,13 @@
+package christinaGerdzhikova.week06;
+
+public class PasswordValidation {
+
+      //1) String - Password Validation Task
+    //Write a return method that can verify if a password is valid or not. Requirements:
+    //1. Password MUST be at least 6 characters and should not contain space
+    //2. Password should at least contain one upper case letter
+    //3. Password should at least contain one lowercase letter
+    //4. Password should at least contain one special characters
+    //5. Password should at least contain a digit
+    //6. if all requirements above are met, the method returns true, otherwise returns false
+}
