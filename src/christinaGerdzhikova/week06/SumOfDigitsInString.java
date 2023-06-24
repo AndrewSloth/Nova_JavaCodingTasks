4ba5204b14c69b5367ca06a2955b1d943f6467b3
Chris_Cydeo <praskofka@hotmail.com>
2023-06-24 13:28:26 -0700

    created sum of digits in string class

---
+++ src/christinaGerdzhikova/week06/SumOfDigitsInString.java
@@ -0,0 +1,7 @@
+package christinaGerdzhikova.week06;
+
+public class SumOfDigitsInString {
+
+     //2) String - sum of digits in a string
+    //Write a method that can return the sum of the digits in a string
+}
