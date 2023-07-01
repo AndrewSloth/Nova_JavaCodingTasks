04e5193c7388be199af9798766bf558a52c0abbf
Chris_Cydeo <praskofka@hotmail.com>
2023-07-01 12:31:00 -0700

    created array sort ascending class

---
+++ src/christinaGerdzhikova/week07/ArraySortAscending.java
@@ -0,0 +1,10 @@
+package christinaGerdzhikova.week07;
+
+import java.util.Arrays;
+
+public class ArraySortAscending {
+
+    //2) Array - Sort Ascending
+    //Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays
+
+}
