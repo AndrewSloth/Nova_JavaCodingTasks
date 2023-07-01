dae683689dade4fd7709593c64a208461f1a5f1e
Chris_Cydeo <praskofka@hotmail.com>
2023-07-01 13:19:49 -0700

    created array sort descending class

---
+++ src/christinaGerdzhikova/week07/ArraySortDescending.java
@@ -0,0 +1,9 @@
+package christinaGerdzhikova.week07;
+
+import java.util.Arrays;
+
+public class ArraySortDescending {
+
+    //3) Array - Sort Descending
+    //Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
+}
