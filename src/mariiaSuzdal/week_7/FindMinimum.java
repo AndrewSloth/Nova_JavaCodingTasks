46af8741f318f9a8268644cfe34b922fef5fae8c
maria_suzdal <m.suzdal11@gmail.com>
2023-07-08 21:35:18 -0700

    added solutions for the all tasks from week 7

--- src/mariiaSuzdal/week_7/FindMinimum.java
+++ src/mariiaSuzdal/week_7/FindMinimum.java
@@ -1,6 +1,34 @@
 package mariiaSuzdal.week_7;
 
 public class FindMinimum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {90, 70, 30, 40, 59};
+        System.out.println(findMin(numbers));
+
+        int[] numbers2 = {-90, 70, -30, 40, 59};
+        System.out.println(findMin(numbers2));
+
+    }
+
+
+    public static int findMin(int[] listNumbers) {
+
+        //created int with min value of the first element of the array
+        int newList = listNumbers[0];
+        //created a for each loop for "listNumbers"
+        for (int each : listNumbers) {
+            //if each less than newList in array I assigned it in newList
+            if (each < newList) {
+                newList = each;
+            }
+
+        }
+        return newList;
+    }
+
+
 }
 /*
 Array - Find Minimum
--- src/mariiaSuzdal/week_7/SortDescending.java
+++ src/mariiaSuzdal/week_7/SortDescending.java
@@ -1,6 +1,38 @@
 package mariiaSuzdal.week_7;
 
+import java.util.Arrays;
+
 public class SortDescending {
+
+    public static void main(String[] args) {
+
+        int[] arr = {-10, 900, 8, 78, -1, 0};
+        System.out.println(Arrays.toString(sortAscending(arr)));
+
+    }
+
+    public static int[] sortAscending(int[] arr) {
+
+//I create a first loop that starts from the first element and go through the whole array
+        for (int i = 0; i < arr.length; i++) {
+//I create a second loop that starts from the first element and go through the array except the last element
+            for (int j = 0; j < arr.length - 1; j++) {
+//I create if statement where I check if the value of j LESS than next j value
+                if (arr[j] < arr[j + 1]) {
+//I create a temporary variable where I will store j value that I will swap
+                    int temp = arr[j];
+//I swapped first and second j value by assign second j value to the first j value
+                    arr[j] = arr[j + 1];
+//I reassigned temp value into second j value
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+
+        return arr;
+    }
+
 }
 /*
 Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
