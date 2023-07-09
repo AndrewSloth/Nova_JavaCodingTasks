f804bbc8524cbace95fb1e5dae43108c65474a92
maria_suzdal <m.suzdal11@gmail.com>
2023-07-08 20:33:31 -0700

    created new files for the all tasks from week 8

---
+++ src/mariiaSuzdal/week_8/Array_MoveZerosToTheEnd.java
@@ -0,0 +1,9 @@
+package mariiaSuzdal.week_8;
+
+public class Array_MoveZerosToTheEnd {
+}
+/*
+Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method)
+input:  {1,0,2,0,3,0,4,0};
+output: [1, 2, 3, 4, 0, 0, 0, 0]
+ */
