c97e0a857a1727542f76040b90057c41d3832394
nataliiachobu <mebel2008@yahoo.com>
2023-07-11 09:40:56 -0700

    week8 package

--- src/nataliia_chobu/week8/Array_NuniqueIntegersThatSumUpTo.java
+++ src/nataliia_chobu/week8/Array_NuniqueIntegersThatSumUpTo.java
@@ -10,7 +10,6 @@ public class Array_NuniqueIntegersThatSumUpTo {
 //The function can return any such array. For example, given N = 4, the function could return [1, 0, -3, 2] or [-2, 1, -4, 5].
 //The answer [1, -1, 1, 3] would be incorrect (because value 1 occurs twice).
 //For N = 3 one of the possible answers is [-1,0,1]  (but there are many more correct answers).
-    //Chat GPT solution
 
     public static void main(String[] args) {
 
