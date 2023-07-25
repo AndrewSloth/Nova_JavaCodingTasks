7664bff1ef81690c355fee203075d0b3033525c3
Chris_Cydeo <praskofka@hotmail.com>
2023-07-25 10:39:38 -0700

    finished remove ahmed class

--- src/christinaGerdzhikova/week09/List_RemoveNameAhmed.java
+++ src/christinaGerdzhikova/week09/List_RemoveNameAhmed.java
@@ -1,4 +1,20 @@
 package christinaGerdzhikova.week09;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class List_RemoveNameAhmed {
+
+    public static void removeAhmed(ArrayList<String> names){
+        names.removeIf(p -> p.equals("Ahmed"));
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList("Ahmed", "We", "Need", "To", "Cook"));
+        removeAhmed(list);
+
+        System.out.println(list);
+    }
 }
