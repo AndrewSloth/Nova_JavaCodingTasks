7ca89d54a9b7015cc0025d8366f2db7743b7f343
Chris_Cydeo <praskofka@hotmail.com>
2023-07-25 10:49:54 -0700

    finished remove some values class

--- src/christinaGerdzhikova/week09/List_RemoveSomeValues.java
+++ src/christinaGerdzhikova/week09/List_RemoveSomeValues.java
@@ -1,4 +1,28 @@
 package christinaGerdzhikova.week09;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class List_RemoveSomeValues {
+
+    public static ArrayList<Integer> removeValues(ArrayList<Integer> list){
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (Integer each : list) {
+            if (each > 100){
+                continue;
+            }
+            result.add(each);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 101, 200, 300));
+
+        System.out.println(removeValues(list));
+    }
 }
