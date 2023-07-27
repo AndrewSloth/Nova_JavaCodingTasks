1d2c418132a29af7c0a1cf8e9a2bcbb9c24cfe38
yusufyalinlar <yusufyailnlar@gmail.com>
2023-07-27 16:58:51 -0700

    Find Maximum entry #1

--- src/JosephYalinlar/week6/Array_FindMaximum.java
+++ src/JosephYalinlar/week6/Array_FindMaximum.java
@@ -1,4 +1,12 @@
 package JosephYalinlar.week6;
-
+//Write a method that can find the maximum number from an int Array
 public class Array_FindMaximum {
+    public int findMaximum(int[] arr){
+        arr[].max
+        return maximumNum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 10, 4, 5, 6};
+    }
 }
